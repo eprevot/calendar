86f1ed0233ae126967a3e73aa4b6858ed6d8e358
eprevot <eprevot@patschool.com>
2015-01-20 11:32:06 +0100

    add some events to display

--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -12,7 +12,12 @@ angular.module('calendarApp')
     return {
       getEvents : function(/*$http*/) {
             //here I use $http to get the list of events from server
-            var events = ['lune', 'mars', 'mercure', 'jupiter', 'venus', 'saturne', 'soleil'];
+            var events = [
+            				{startDate: new Date(2014, 9, 8, 9, 30), endDate: new Date(2014, 9, 8, 14, 0), title: 'Al Di Meola'},
+					    	{startDate: new Date(2014, 9, 10, 9, 15), endDate: new Date(2014, 9, 10, 12, 30), title: 'Paco De Lucia'},
+					    	{startDate: new Date(2014, 9, 10, 12, 0), endDate: new Date(2014, 9, 10, 15, 30), title: 'John McLaughlin'},
+					    	{startDate: new Date(2014, 9, 10, 15, 0), endDate: new Date(2014, 9, 10, 18, 30), title: 'Django Reinhardt'}
+					    ];
             return events;
       }
     };
