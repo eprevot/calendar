1e04ec3e39fdee47c40d6b878dde95063ff03a81
eprevot <eprevot@patschool.com>
2015-01-20 18:59:04 +0100

    update events service to create displayable div from Event data (TO FINISH)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,19 @@ function Day(timestamp, content) {
 }
 
 
+function Event(startDate, endDate, title) {
+	//public methods
+	this.getTitle = function() {
+		return title;
+	};
+	this.getPercentHeight = function() {
+		return 100 * (endDate - startDate)/86400000;//1 day = 24*60*60*1000 ms
+	};
+	this.getPercentTop = function() {
+		return 100 * (startDate.getHours * 60 + startDate.getMinutes * 60)/1440;//1 day = 24*60 min
+	};
+}
+
 /**
  * @ngdoc function
  * @name calendarApp.controller:MainCtrl
@@ -20,11 +33,18 @@ function Day(timestamp, content) {
  */
 angular.module('calendarApp')
 	.controller('MainCtrl', function($scope, week, events) {
+
+		function displayEvents(events) {
+			for (var i = 0; i < events.length; i++) {
+				var event = new Event(events[i]);
+				event.getTitle
+			}			
+		}
+
 		var daysTime = week.getDays();
-		var daysEvents = events.getEvents();
 		$scope.days = [];
 		for (var i = 0; i < 7; i++) {
-			$scope.days[i] = new Day(daysTime[i], daysEvents[i]);
+			$scope.days[i] = new Day(daysTime[i], events.getEventsForDay(daysTime[i]));
 		}
 	});
 
--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -9,16 +9,48 @@
  */
 angular.module('calendarApp')
   .factory('events', function (/*$http*/) {
+
+  	function getAllEvents() {
+        //here I use $http to get the list of events from server
+        var events = [
+			new Event( new Date(2014, 9, 8, 9, 30), new Date(2014, 9, 8, 14, 0), 'Al Di Meola'),
+	    	new Event( new Date(2014, 9, 10, 9, 15), new Date(2014, 9, 10, 12, 30), 'Paco De Lucia'),
+	    	new Event( new Date(2014, 9, 10, 12, 0), new Date(2014, 9, 10, 15, 30), 'John McLaughlin'),
+	    	new Event( new Date(2015, 0, 21, 15, 0), new Date(2015, 0, 21, 18, 30), 'Django Reinhardt')
+		];
+        return events;
+    }
+
+    function Event(startDate, endDate, title) {
+		//public methods
+		this.getStartDate = function() {
+			return startDate;
+		}
+		this.getEndDate = function() {
+			return endDate;
+		}
+		this.getTitle = function() {
+			return title;
+		};
+		this.getPercentHeight = function() {
+			return 100 * (endDate - startDate)/86400000;//1 day = 24*60*60*1000 ms
+		};
+		this.getPercentTop = function() {
+			return 100 * (startDate.getHours * 60 + startDate.getMinutes * 60)/1440;//1 day = 24*60 min
+		};
+	}
+
+
     return {
-      getEvents : function(/*$http*/) {
-            //here I use $http to get the list of events from server
-            var events = [
-            				{startDate: new Date(2014, 9, 8, 9, 30), endDate: new Date(2014, 9, 8, 14, 0), title: 'Al Di Meola'},
-					    	{startDate: new Date(2014, 9, 10, 9, 15), endDate: new Date(2014, 9, 10, 12, 30), title: 'Paco De Lucia'},
-					    	{startDate: new Date(2014, 9, 10, 12, 0), endDate: new Date(2014, 9, 10, 15, 30), title: 'John McLaughlin'},
-					    	{startDate: new Date(2014, 9, 10, 15, 0), endDate: new Date(2014, 9, 10, 18, 30), title: 'Django Reinhardt'}
-					    ];
-            return events;
+      getEventsForDay : function(date) {
+      	var events = getAllEvents();
+      	var dayEvents = [];
+      	for (var i = 0; i < events.length; i++) {
+      		if(events[i].startDate.toDateString() === date.toDateString() || events[i].endDate.toDateString() === date.toDateString()) {
+      			dayEvents.push(events[i]);
+      		}
+      	}
+      	return dayEvents;
       }
     };
   });
