079cca07f1ae2838149b9e7ea4aee54865a8a426
eprevot <eprevot@patschool.com>
2015-01-20 19:00:22 +0100

    update events service to create displayable div from Event data (TO FINISH)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,18 +11,6 @@ function Day(timestamp, content) {
 }
 
 
-function Event(startDate, endDate, title) {
-	//public methods
-	this.getTitle = function() {
-		return title;
-	};
-	this.getPercentHeight = function() {
-		return 100 * (endDate - startDate)/86400000;//1 day = 24*60*60*1000 ms
-	};
-	this.getPercentTop = function() {
-		return 100 * (startDate.getHours * 60 + startDate.getMinutes * 60)/1440;//1 day = 24*60 min
-	};
-}
 
 /**
  * @ngdoc function
@@ -35,6 +23,7 @@ angular.module('calendarApp')
 	.controller('MainCtrl', function($scope, week, events) {
 
 		function displayEvents(events) {
+			var divs = [];
 			for (var i = 0; i < events.length; i++) {
 				var event = new Event(events[i]);
 				event.getTitle
