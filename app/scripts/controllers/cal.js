675fe98e94c5322edc0da7f55315eedf8ac5c5b3
eprevot <eprevot@patschool.com>
2015-02-13 15:06:42 +0100

    buttons to change displaying week

--- app/scripts/controllers/cal.js
+++ app/scripts/controllers/cal.js
@@ -19,14 +19,30 @@ function Day(timestamp, events) {
  */
 angular.module('calendarApp')
 	.controller('CalCtrl', function($scope, week, events) {
+
+		function setDays() {
+			$scope.currentDate = currentDate;
+			$scope.days = [];
+			for (var i = 0; i < 7; i++) {
+				$scope.days[i] = new Day(daysTime[i], events.getEventsForDay(daysTime[i]));
+			}
+		}
+
+		$scope.prevWeek = function() {
+			currentDate.setDate(currentDate.getDate() -7);
+			daysTime = week.getDays(currentDate);
+			setDays();
+		};
+
+		$scope.nextWeek = function() {
+			currentDate.setDate(currentDate.getDate() +7);
+			daysTime = week.getDays(currentDate);
+			setDays();
+		};
+
 		var currentDate = new Date();
 		var daysTime = week.getDays(currentDate);
-
-		$scope.currentDate = currentDate;
-		$scope.days = [];
-		for (var i = 0; i < 7; i++) {
-			$scope.days[i] = new Day(daysTime[i], events.getEventsForDay(daysTime[i]));
-		}
+		setDays();
 	});
 
 
